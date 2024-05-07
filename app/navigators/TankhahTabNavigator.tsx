ffa70854f472c68d0835b3b5e4b21d104fe68dc4
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-05-07 18:48:57 +0330

    remove unused code

--- app/navigators/TankhahTabNavigator.tsx
+++ app/navigators/TankhahTabNavigator.tsx
@@ -1,16 +1,12 @@
-import { CompositeScreenProps, NavigatorScreenParams } from "@react-navigation/native"
+import { CompositeScreenProps } from "@react-navigation/native"
 import React from "react"
-import { TextStyle, ViewStyle } from "react-native"
 import { Icon } from "../components"
 import { translate } from "../i18n"
-import { colors, spacing, typography } from "../theme"
 import { AppStackParamList, AppStackScreenProps } from "./AppNavigator"
 import * as Screens from "app/screens"
 
-import { ChargeNavigator, ChargeStackParamList } from "./ChargeNavigator"
 import {
   createMaterialBottomTabNavigator,
-  MaterialBottomTabNavigationProp,
   MaterialBottomTabScreenProps,
 } from "react-native-paper/react-navigation"
 
@@ -31,10 +27,6 @@ export type TankhahTabScreenProps<T extends keyof TankhahTabParamList> = Composi
 
 const Tab = createMaterialBottomTabNavigator<TankhahTabParamList>()
 
-const animationConfig = {
-  animation: 'timing',
-  config: { duration: 0 },
-};
 /**
  * This is the main navigator for the Tankhah screens with a bottom tab bar.
  * Each tab is a stack navigator with its own set of screens.
