c363782e0b85ea339cb9ecca2a7a8f7d988033b9
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2024-05-28 18:01:46 +0330

    remove old screen

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -3,7 +3,6 @@ export * from "./LoginScreen"
 export * from "./ErrorScreen/ErrorBoundary"
 // export other screens here
 export * from "./TankhahScreen/TankhahHomeScreen"
-export * from "./ChargeScreen/TankhahChargeListScreen"
 export * from "./TankhahScreen/TankhahChargeFormScreen"
 export * from "./TankhahScreen/TankhahItemScreen"
 export * from "./TankhahScreen/SpendFormScreen"
